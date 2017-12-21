b89adc05a0397d9f8fc95eadfd7e32e389aa7ae7
Brendan Duke <dukebw@mcmaster.ca>
2017-12-21 09:18:09 -0500

    Add setup.py script for developer install of Dominion.

--- play_game.py
+++ play_game.py
@@ -2,19 +2,25 @@ from game import Game
 from agent import RandomAgent
 from cards import *
 
-agent_dict = {'DemoBot 1': RandomAgent()}
-demo_game = Game(n_players=3, agents=agent_dict, card_set='base', verbose=True)
 
-demo_game.players[0].display_deck()
-print("")
+def play_game():
+    agent_dict = {'DemoBot 1': RandomAgent()}
+    demo_game = Game(n_players=3, agents=agent_dict, card_set='base', verbose=True)
 
-demo_game.players[0].display_hand()
-print("")
+    demo_game.players[0].display_deck()
+    print("")
 
-demo_game.players[0].display_draw_pile()
-print("")
+    demo_game.players[0].display_hand()
+    print("")
 
-demo_game.players[0].display_discard_pile()
-print("")
+    demo_game.players[0].display_draw_pile()
+    print("")
 
-scores = demo_game.play_game()
+    demo_game.players[0].display_discard_pile()
+    print("")
+
+    scores = demo_game.play_game()
+
+
+if __name__ == '__main__':
+    play_game()
